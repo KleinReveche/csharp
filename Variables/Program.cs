24316443010e7606e31e07c7484a861a29623946
Klein Reveche <therealklr2003@gmail.com>
2023-08-14 10:28:02 +0800

    [Variables]

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 2;
+            int y = 3;
+            float z = 4;
+            char c = 'K';
+            string name = "Klein";
+            bool isDone = true;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(c);
+            Console.WriteLine(name);
+            Console.WriteLine(isDone);
+        }
+    }
+}
