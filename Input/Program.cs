769a521389602cf093a822a4dc9b5c75467d22bf
Klein Reveche <therealklr2003@gmail.com>
2023-08-14 11:00:38 +0800

    [Input]

---
+++ Input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int age;
+
+            Console.Write("What's your first name? ");
+            firstName = Console.ReadLine();
+            Console.Write("What's your last name? ");
+            lastName = Console.ReadLine();
+            Console.Write("What's your age? ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Hello " + firstName + " " + lastName + "!");
+            Console.WriteLine("Your age is " + age);
+        }
+    }
+}
