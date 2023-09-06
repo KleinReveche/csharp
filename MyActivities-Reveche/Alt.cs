0c1c4bdcf00a8637f05c2b6d682b5b6a8cedd398
Klein Reveche <therealklr2003@gmail.com>
2023-09-06 08:48:20 +0800

    [MyActivities-Reveche] Consolidated Activities

---
+++ MyActivities-Reveche/Alt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyActivities_Reveche
+{
+    public class Alt
+    {
+        public static void HighestLowestArrays()
+        {
+            // with use of Arrays
+            var numbers = new int[5];
+
+            Console.WriteLine("Input 5 Numbers:");
+            Console.Write("> ");
+            numbers[0] = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            numbers[1] = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            numbers[2] = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            numbers[3] = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            numbers[4] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The highest value is {0}.", numbers.Max());
+            Console.WriteLine("The lowest value is {0}.", numbers.Min());
+        }
+
+        public static void HighestLowestLists()
+        {
+            // with use of Lists
+            var numbers = new List<int>();
+
+            Console.WriteLine("Input 5 Numbers:");
+            Console.Write("> ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+            Console.Write("> ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+            Console.Write("> ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+            Console.Write("> ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+            Console.Write("> ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("The highest value is {0}.", numbers.Max());
+            Console.WriteLine("The lowest value is {0}.", numbers.Min());
+        }
+    }
+}
---
+++ MyActivities-Reveche/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace MyActivities_Reveche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetic();
+            //Grade();
+            HighestLowest();
+
+            // These are extra alternative implementations of the activities
+            //Alt.HighestLowestArrays();
+            //Alt.HighestLowestLists();
+
+        }
+        static void Arithmetic()
+        {
+            int x, y;
+            float result = 0;
+            char optr;
+            string optrResult = "";
+            bool isValid = true;
+
+            Console.WriteLine("Welcome to SuperBasicCalculatorRedux!");
+            Console.WriteLine("Enter your number to get started. \n");
+
+            Console.Write("Enter number 1: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter arithmetic operator: ");
+            optr = Convert.ToChar(Console.ReadLine());
+
+            switch (optr)
+            {
+                case '+':
+                    result = x + y;
+                    optrResult = "sum";
+                    break;
+                case '-':
+                    result = x - y;
+                    optrResult = "difference";
+                    break;
+                case '*':
+                    result = x * y;
+                    optrResult = "product";
+                    break;
+                case '/':
+                    result = (float)x / y;
+                    optrResult = "quotient";
+                    break;
+                default:
+                    isValid = false;
+                    break;
+            }
+
+            if (isValid)
+                Console.WriteLine("\nThe {0} of {1} {2} {3} is {4}", optrResult, x, optr, y, result);
+            else
+                Console.WriteLine("The operator is invalid.");
+        }
+
+        static void Grade()
+        {
+            string fullName;
+            string equivGrade;
+            double grade = 0;
+            bool isValid = false;
+
+            Console.Write("Full Name: ");
+            fullName = Console.ReadLine();
+
+            while (!isValid)
+            {
+                Console.Write("Grade: ");
+                if (double.TryParse(Console.ReadLine(), out grade) && grade <= 100)
+                    isValid = true;
+            }
+
+            if (grade >= 98)
+                equivGrade = "1.0";
+            else if (grade >= 95)
+                equivGrade = "1.25";
+            else if (grade >= 92)
+                equivGrade = "1.50";
+            else if (grade >= 89)
+                equivGrade = "1.0";
+            else if (grade >= 86)
+                equivGrade = "2.0";
+            else if (grade >= 83)
+                equivGrade = "2.25";
+            else if (grade >= 80)
+                equivGrade = "2.50";
+            else if (grade >= 77)
+                equivGrade = "2.75";
+            else if (grade == 75)
+                equivGrade = "3.00";
+            else
+                equivGrade = "5.0";
+
+            Console.WriteLine("{0}, your grade is {1}.", fullName, equivGrade);
+        }
+
+        static void HighestLowest()
+        {
+            // Basic
+            int a, b, c, d, e;
+            int max = 0, min = 0;
+
+            Console.WriteLine("Input 5 Numbers:");
+            Console.Write("> ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("> ");
+            e = int.Parse(Console.ReadLine());
+
+            if (a >= b && a >= c && a >= d && a >= e) max = a;
+            if (b >= c && b >= d && b >= e && b >= a) max = b;
+            if (c >= d && c >= e && c >= a && c >= b) max = c;
+            if (d >= e && d >= a && d >= b && d >= c) max = d;
+            if (e >= a && e >= b && e >= c && d >= b) max = e;
+
+            if (a <= b && a <= c && a <= d && a <= e) min = a;
+            if (b <= c && b <= d && b <= e && b <= a) min = b;
+            if (c <= d && c <= e && c <= a && c <= b) min = c;
+            if (d <= e && d <= a && d <= b && d <= c) min = d;
+            if (e <= a && e <= b && e <= c && d <= b) min = e;
+
+            Console.WriteLine("The highest value is {0}.", max);
+            Console.WriteLine("The lowest value is {0}.", min);
+        }
+
+    }
+}
