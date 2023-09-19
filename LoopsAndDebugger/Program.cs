13cae00f3b29a263fee59c49c8f5edfd624f90f5
Klein Reveche <therealklr2003@gmail.com>
2023-09-19 17:26:26 +0800

    [LoopsAndDebugger] tutorial session

---
+++ LoopsAndDebugger/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace LoopsAndDebugger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoop();
+            //WhileLoop();
+            //DoWhileLoop();
+            //FizzBuzzExample();
+            Solution();
+        }
+
+        static void ForLoop()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void WhileLoop()
+        {
+            while (true)
+            {
+                Console.Write("Give a number: ");
+                var input = int.Parse(Console.ReadLine());
+
+                if (input == 5)
+                    break;
+            }
+
+        }
+
+        static void DoWhileLoop()
+        {
+
+            do
+            {
+                Console.Write("Give a number: ");
+                var input = int.Parse(Console.ReadLine());
+
+                if (input == 5)
+                    break;
+            }
+            while (true);
+
+        }
+
+        static void FizzBuzzExample()
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                    Console.WriteLine("FIZZBUZZ");
+
+                else if (number % 3 == 0)
+                    Console.WriteLine("FIZZ");
+
+                else if (number % 5 == 0)
+                    Console.WriteLine("BUZZ");
+
+                else Console.WriteLine(number);
+            }
+        }
+
+        static void Solution()
+        {
+            var oddEvenResult = "";
+            var num = 5;
+
+            for (var i = 1; i <= num * 2; i++)
+            {
+                if (i % 2 == 0) 
+                    oddEvenResult += $"{i - 1 + i}, ";
+            }
+            Console.WriteLine(oddEvenResult.Substring(0, oddEvenResult.Length - 2));
+        }
+    }
+}
