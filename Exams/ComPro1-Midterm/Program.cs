d3135f3c45c35ef646acb8494b2c4c4c553beb0f
Klein Reveche <therealklr2003@gmail.com>
2023-09-20 08:21:05 +0800

    [ComPro1-Midterm] Problem One

---
+++ Exams/ComPro1-Midterm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPro1_Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemOne();
+        }
+        static void ProblemOne()
+        {
+            Console.Write("Enter your score: ");
+            var score = int.Parse(Console.ReadLine());
+
+            if (score < 0) Console.WriteLine("No numbers are entered except 0.");
+            else if (score >= 60) Console.WriteLine("You pass the exam.");
+            else Console.WriteLine("You didn't pass the exam");
+        }
+    }
+}
