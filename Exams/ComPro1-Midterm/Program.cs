91cd7f2bc2be2ba59f774fd53ee9b9345c698581
Klein Reveche <therealklr2003@gmail.com>
2023-09-20 09:08:17 +0800

    [ComPro1-Midterm] Problem Three

--- Exams/ComPro1-Midterm/Program.cs
+++ Exams/ComPro1-Midterm/Program.cs
@@ -7,8 +7,10 @@ namespace ComPro1_Midterm
         static void Main(string[] args)
         {
             //ProblemOne();
-            ProblemTwo();
+            //ProblemTwo();
+            ProblemThree();
         }
+
         static void ProblemOne()
         {
             Console.Write("Enter your score: ");
@@ -30,6 +32,50 @@ namespace ComPro1_Midterm
 
             // One line impl.
             //for (var celsius = 0; celsius <= 100; celsius++) Console.WriteLine($"{celsius}        {(celsius * 9 / 5.0) + 32}");
+        }
+
+        static void ProblemThree()
+        {
+            int highest = 0, lowest = int.MaxValue, highestOccurences = 0, lowestOccurences = 0;
+            string numbers = "";
+
+            Console.WriteLine("Enter numbers: ");
+
+            while (true)
+            {
+
+                Console.Write($"> ");
+                var number = int.Parse(Console.ReadLine());
+
+                if (number < 0) continue;
+
+                numbers += $"{number} ";
+
+                if (number == 0) break;
+
+                if (number == highest)
+                    highestOccurences++;
+
+                if (number == lowest)
+                    lowestOccurences++;
+
+                if (number > highest)
+                {
+                    highest = number;
+                    highestOccurences = 1;
+                }
+
+                if (number < lowest)
+                {
+                    lowest = number;
+                    lowestOccurences = 1;
+                }
+            }
+            Console.WriteLine($"\nNumbers are {numbers}");
+            Console.WriteLine($"\nThe highest number is {highest}");
+            Console.WriteLine($"The occurence of the highest number is {highestOccurences}");
+            Console.WriteLine($"The lowest number is {lowest}");
+            Console.WriteLine($"The occurence of the lowest number is {lowestOccurences}");
         }
     }
 }
