27c4b65546bd91ae960772bf65d8a1bb0aaaa8d0
Klein Reveche <therealklr2003@gmail.com>
2023-09-20 08:35:29 +0800

    [ComPro1-Midterm] Problem Two

--- Exams/ComPro1-Midterm/Program.cs
+++ Exams/ComPro1-Midterm/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComPro1_Midterm
 {
@@ -10,7 +6,8 @@ namespace ComPro1_Midterm
     {
         static void Main(string[] args)
         {
-            ProblemOne();
+            //ProblemOne();
+            ProblemTwo();
         }
         static void ProblemOne()
         {
@@ -20,6 +17,19 @@ namespace ComPro1_Midterm
             if (score < 0) Console.WriteLine("No numbers are entered except 0.");
             else if (score >= 60) Console.WriteLine("You pass the exam.");
             else Console.WriteLine("You didn't pass the exam");
+        }
+
+        static void ProblemTwo()
+        {
+            Console.WriteLine("Celsius  Farenheit");
+            for (var celsius = 0; celsius <= 100; celsius++)
+            {
+                double farenheight = (celsius * 9 / 5.0) + 32;
+                Console.WriteLine($"{celsius}        {farenheight}");
+            }
+
+            // One line impl.
+            //for (var celsius = 0; celsius <= 100; celsius++) Console.WriteLine($"{celsius}        {(celsius * 9 / 5.0) + 32}");
         }
     }
 }
