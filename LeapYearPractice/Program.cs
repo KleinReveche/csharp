d5931e90034b06ea22984368aaef478ff1a8ab17
Klein Reveche <therealklr2003@gmail.com>
2023-09-24 18:08:17 +0800

    [LeapYearPractice]

---
+++ LeapYearPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeapYearPractice
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Title = "Leap Year Practice";
+                Console.Write("Enter a year: ");
+                var year = int.Parse(Console.ReadLine() ?? "-1");
+
+                if (year < 0) break;
+                
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                    Console.WriteLine($"{year} is a leap year.\n");
+                else Console.WriteLine($"{year} is not a leap year.\n");
+            }
+        }
+    }
+}
